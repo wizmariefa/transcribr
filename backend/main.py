c3f3c9ccbd8a107fd462273cc6181e2c3f172416
wizmariefa <mariecolee1@gmail.com>
2019-01-26 14:55:52 -0700

    working on database/login methods

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,46 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from werkzeug.utils import secure_filename
 from google_auth import Google_Auth
 from transcription import Transcribr
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+#############################################
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 app.secret_key = os.urandom(24)
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(12), nullable = False)
     lastName = db.Column(db.String(12), nullable = False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     passwordHash = db.Column(db.String(256), nullable = False)
+    registeredOn = db.Column(db.String(120), nullable=False)
     gcpAuthToken = db.Column(db.String(120))
     gcpRefreshToken = db.Column(db.String(120))
 
-    def __repr__(self):
-        return '<User %r>' % self.email
+    def __init__(self, firstName, lastName, email, passwordHash, gcpAuthToken=None, gcpRefreshToken = None):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.passwordHash = passwordHash
+        self.gcpAuthToken = gcpAuthToken
+        self.gcpRefreshToken = gcpRefreshToken
+        self.registered_on = datetime.utcnow()
 
-@app.route("/")
-def main():
+
+#############################################
+
+@app.route("/login")
+def login():
+    # login user with password
+    pass
+
+@app.route("/signup")
+def sign_up():
+    # sign up new user
     pass
 
 @app.route("/auth/register")
@@ -32,7 +50,27 @@ def auth_register():
 
 @app.route("/auth/login")
 def auth_login():
+    # this will allow login with google auth
     pass
+
+@app.route("/transcribe", methods=["GET", "POST"])
+def fileupload():
+    # this will be called to upload and transcribe file
+    UPLOAD_FOLDER = '/path/to/the/uploads'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+    target = os.path.join(UPLOAD_FOLDER, 'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    destination = "/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath'] = destination
+    response = "Whatever you wish too return"
+    return response
+
+#############################################
 
 if __name__ == "__main__":
     app.run()
