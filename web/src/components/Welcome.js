c7ed094ef15406e3d0f86703bd793aa6f39e0c01
Mike Pollard <mpollard@wolfssl.com>
2019-01-26 17:06:51 -0700

    react error

--- web/src/components/Welcome.js
+++ web/src/components/Welcome.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
-import './../styles/Welcome.scss';
-import { Link } from "react-router-dom";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-    };
-  }
+export default class ConnectGoogleCloud extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to BeanFest. Choose an opton below.</h1>
-        <div className='WelcomeOptions FlexRow JustifyCenter AlignCenter'>
-          <Link to="/study" className="Flex1 FlexRow JustifyCenter AlignCenter LargeText">
-            Play BeanFest
-          </Link>
-          <Link to="/login" className="Flex1 FlexRow JustifyCenter AlignCenter LargeText">
-            Create a Study
-          </Link>
-        </div>
-      </div>
+      <a href={getOAuthUrl('http://localhost:3000')}>
+        <div className="Button">Connect Google Account</div>
+      </a>
     );
   }
 }
-
-export default App;
